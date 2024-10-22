9fda851546644a7183921ed614eb7ed3c3506c03
Matt Castoro <matthewjcastoro@gmail.com>
2024-10-22 14:19:13 -0600

    Store player names and selections
    Write functions to store players' names and their respective
    selections on the gameboard.

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+// (function() {
+
+// })();
 
 const boxArray = document.querySelectorAll(".gameBox");
 boxArray.forEach((btn) => {
     btn.addEventListener("click", (event) => {
-        document.getElementById(event.target.id).textContent = "X";
+        if (displayStatement.textContent == `${playerX}'s turn...`) {
+            document.getElementById(event.target.id).textContent = "X";
+            gameboard.xKey.push(event.target.id);
+            displayStatement.textContent = `${playerO}'s turn...`;
+        } else if (displayStatement.textContent == `${playerO}'s turn...`) {
+            document.getElementById(event.target.id).textContent = "O";
+            gameboard.oKey.push(event.target.id);
+            displayStatement.textContent = `${playerX}'s turn...`;
+        }
+        
     })
 })
+
+const startGameBtn = document.getElementById("startGame");
+startGameBtn.addEventListener("click", () => {
+    startGame();
+})
+
+let playerX;
+let playerO;
+let displayStatement = document.querySelector(".statement");
+function startGame() {
+    playerX = document.querySelector("#playerXName").value;
+    playerO = document.querySelector("#playerOName").value;
+    displayStatement.textContent = `${playerX}'s turn...`;
+}
+
+const gameboard = {};
+gameboard.xKey = new Array();
+gameboard.oKey = new Array();
+
