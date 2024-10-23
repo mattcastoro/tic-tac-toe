832df46b3ae01b461a7c71b4dbda696238edbca0
Matt Castoro <matthewjcastoro@gmail.com>
2024-10-23 10:39:07 -0600

    Add gameResult function
    Create function that determines if a player's selections has made them
    win the game.

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ boxArray.forEach((btn) => {
         if (displayStatement.textContent == `${playerX}'s turn...`) {
             document.getElementById(event.target.id).textContent = "X";
             gameboard.xKey.push(event.target.id);
+            gameResult(gameboard.xKey);
             displayStatement.textContent = `${playerO}'s turn...`;
         } else if (displayStatement.textContent == `${playerO}'s turn...`) {
             document.getElementById(event.target.id).textContent = "O";
             gameboard.oKey.push(event.target.id);
+            gameResult(gameboard.oKey);
             displayStatement.textContent = `${playerX}'s turn...`;
         }
         
@@ -27,6 +29,7 @@ let playerX;
 let playerO;
 let displayStatement = document.querySelector(".statement");
 function startGame() {
+    //validate users have added their names //
     playerX = document.querySelector("#playerXName").value;
     playerO = document.querySelector("#playerOName").value;
     displayStatement.textContent = `${playerX}'s turn...`;
@@ -35,4 +38,24 @@ function startGame() {
 const gameboard = {};
 gameboard.xKey = new Array();
 gameboard.oKey = new Array();
+
+const winningCombos = [
+    ["gameBox11", "gameBox12","gameBox13"],
+    ["gameBox11", "gameBox22","gameBox33"],
+    ["gameBox11", "gameBox21","gameBox31"],
+    ["gameBox12", "gameBox22","gameBox32"],
+    ["gameBox13", "gameBox23","gameBox33"],
+    ["gameBox13", "gameBox22","gameBox31"],
+    ["gameBox21", "gameBox22","gameBox23"],
+    ["gameBox31", "gameBox32","gameBox33"]
+];
+
+function gameResult(playerSelections) {
+    for (i = 0; i < winningCombos.length; i++) {
+        if (winningCombos[i].every(j => playerSelections.includes(j)) === true) {
+            console.log("test");
+        }
+    }
+}
+
 
